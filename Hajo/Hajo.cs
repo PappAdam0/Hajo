dfaba4bfb7b07eea372ba01f61567e39b94e51c3
Ádám Papp <papp.adaam@gmail.com>
2020-10-11 10:58:05 +0200

    Add project files.

---
+++ Hajo/Hajo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hajo
+{
+    class Hajo
+    {
+        public int Regiszter { get; private set; }
+        public string Nev { get; private set; }
+        public string Tipus { get; private set; }
+        public int Utas { get; private set; }
+        public int Dij { get; private set; }
+        public Hajo(string sor)
+        {
+            //"7;Durbincs;WIN-22;5;24000"
+            string[] adat = sor.Split(';');
+            Regiszter = int.Parse(adat[0]);
+            Nev = adat[1];
+            Tipus = adat[2];
+            Utas = int.Parse(adat[3]);
+            Dij = int.Parse(adat[4]);
+        }
+    }
+}
---
+++ Hajo/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hajo
+{
+    class Program
+    {
+        /*Hány hajó van?
+         * Melyik hajók a legdrágábbak?
+         * Nevsorban a hajók neve "hajok.txt"be
+         * Melyik hajóból (tipus) van a legtöbb*/
+        static List<Hajo> hajok = new List<Hajo>();
+        static List<string> nevek = new List<string>();
+        static void beolvas()
+        {
+            StreamReader olvas = new StreamReader("hajo.csv");
+            while (!olvas.EndOfStream)
+            {
+                //string[] sor = olvas.ReadLine().Split(';');
+                hajok.Add(new Hajo(olvas.ReadLine()));
+            }
+            olvas.Close();
+        }
+        static void HajokSzama()
+        {
+            Console.WriteLine("Hajók száma: {0}", hajok.Count);
+        }
+        static void Legdragabb()
+        {
+            int max = 0;
+            string nev = "";
+            foreach (var i in hajok)
+            {
+                if (i.Dij > max)
+                {
+                    max = i.Dij;
+                    nev = i.Nev;
+                }
+            }
+            Console.Write("{0} {1}", nev, max);
+        }
+        static void Nevsor()
+        {
+            foreach (var i in hajok)
+            {
+                nevek.Add(i.Nev);
+                nevek.Sort();
+            }
+            StreamWriter iro = new StreamWriter("hajok.txt");
+            foreach (var u in nevek)
+            {
+                iro.WriteLine(u);
+            }
+            iro.Close();
+        }
+        static void LegtobbHajo()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            var h = new Hajo("7;Durbincs;WIN-22;5;24000");
+            //Console.Write(h.Nev + " " + h.Tipus);
+            beolvas();
+            HajokSzama();
+            Legdragabb();
+            Nevsor();
+            LegtobbHajo();
+            Console.ReadKey();
+        }
+    }
+}
